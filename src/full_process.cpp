7a28a62dd737d2553bddbca5a56d276babe1e489
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-02-18 11:52:40 +0100

    Clean up and structure thoughts on further steps

--- src/full_process.cpp
+++ src/full_process.cpp
@@ -109,20 +109,18 @@ int main(int argc, char **argv) {
         if (fake3dArg.isSet())
             input_parameters.dimension = 2;
 
-//        std::cout << "Parameters: k=" << input_parameters.k << "\n";
-//        std::cout << "Parameters: denoise_preserve=" << denoise_preserveArg.getValue() << ", denoise_planar="
-//                  << denoise_planarArg.getValue() << ", initial_radius=" << input_parameters.initial_radius << "\n";
 
         ma_data madata = {};
 
         PointList coords;
 
-        int THRES = pointsToProcess.getValue();
+        int NumPointsToProcessPerBatch = pointsToProcess.getValue();
 
         for (std::string line; std::getline(std::cin, line);) {
             std::vector<float> splitLine = split(line, " ");
 
             // Has x, y, z in line
+            // TODO: Might not be necessary anymore? Dependant on input format.
             if (splitLine.size() == 3) {
 
                 Point newPoint = Point(splitLine[0], splitLine[1], splitLine[2]);
@@ -134,151 +132,61 @@ int main(int argc, char **argv) {
                 coords.push_back(newPoint);
                 madata.bbox.addPoint(newPoint);
 
-            } else {
-//                madata.m = splitLine[0];
-//                coords.resize(madata.m);
-//                normals.resize(madata.m);
             }
 
             // When threshold is reached; process available points and dump result to stdout
-            if (coords.size() == THRES) {
+            if (coords.size() == NumPointsToProcessPerBatch) {
 
-                madata.m = THRES;
+                // TODO: Cleanup this assignment
+                madata.m = NumPointsToProcessPerBatch;
 
-                VectorList normals(THRES);
+                VectorList normals(NumPointsToProcessPerBatch);
 
                 madata.normals = &normals;
                 madata.coords = &coords;
 
-//                std::cout << "PROCESSING " << coords.size() << " points" << "\n";
-
+                // Stage 1: Compute the normals of all points currently in memory
                 compute_normals(input_parameters, madata);
 
                 // Storage space for our results:
                 PointList ma_coords(2 * madata.m);
-
-//                std::cout << "NORMALS CALCULATED" << "\n";
 
                 madata.coords = &coords;
                 madata.normals = &normals;
                 madata.ma_coords = &ma_coords;
                 madata.ma_qidx = new int[2 * madata.m];
 
+                // Stage 2: Compute the MASB
                 compute_masb_points(input_parameters, madata);
-
-//                std::cout << "MASB CALCULATED" << "\n";
 
                 madata.lfs = new float[madata.m];
                 madata.mask = new bool[madata.m];
 
-//                std::cout << "bbox: " << madata.bbox.min[0] << " " << madata.bbox.min[1] << " " << madata.bbox.min[2] << " "
-//                  << madata.bbox.max[0] << " " << madata.bbox.max[1] << " " << madata.bbox.max[2] << std::endl;
-
+                // Stage 3: Simplify the LFS of the points
                 simplify_lfs(input_parameters, madata);
 
-//                std::cout << "SIMPLIFIED!!" << "\n";
-
+                // Stage 4: Release points remaining after simplification to stdout
+                // TODO: Do something with point offset?
                 for (int i = 0; i < madata.m; i++) {
                     if (madata.mask[i]) {
                         std::cout << coords[i][0] << " " << coords[i][1] << " " << coords[i][2] << std::endl;
                     }
                 }
 
+                // Delete all points that have been processed in this batch to start fresh
                 coords.resize(0);
 
             }
 
         }
 
-//        cnpy::NpyArray coords_npy = cnpy::npy_load(input_coords_path);
-//        auto *coords_carray = reinterpret_cast<float *>(coords_npy.data);
-//
-//        madata.m = coords_npy.shape[0];
-//        PointList coords(madata.m);
-//
-//        for (unsigned int i = 0; i < madata.m; i++) {
-//            coords[i] = Point(&coords_carray[i * 3]);
-//        }
-
-//        VectorList normals(madata.m);
-//        madata.normals = &normals;
-//        madata.coords = &coords;
-
-//        // Perform the actual processing
-//        compute_normals(input_parameters, madata);
-//
-//        // Output results
-//        auto *normals_carray = new Scalar[madata.m * 3];
-//        for (int i = 0; i < normals.size(); i++)
-//            for (int j = 0; j < 3; j++)
-//                normals_carray[i * 3 + j] = normals[i][j];
-//
-//        const auto c_size = (unsigned int) normals.size();
-//        const unsigned int shape[] = {c_size, 3};
-//
-//        madata.m = coords_npy.shape[0];
-//
-//        // Storage space for our results:
-//        PointList ma_coords(2 * madata.m);
-//
-//        madata.coords = &coords;
-//        madata.normals = &normals;
-//        madata.ma_coords = &ma_coords;
-//        madata.ma_qidx = new int[2 * madata.m];
-//
-//        // Perform the actual processing
-//        compute_masb_points(input_parameters, madata);
-//
-//        // Write out the results for the inside
-//        auto *ma_coords_in_carray = new Scalar[madata.m * 3];
-//        for (int i = 0; i < madata.m; i++)
-//            for (int j = 0; j < 3; j++)
-//                ma_coords_in_carray[i * 3 + j] = ma_coords[i][j];
-//
-//        // Write out the results for the outside
-//        auto *ma_coords_out_carray = new Scalar[madata.m * 3];
-//        for (int i = 0; i < madata.m; i++)
-//            for (int j = 0; j < 3; j++)
-//                ma_coords_out_carray[i * 3 + j] = ma_coords[i + madata.m][j];
-//
-//        madata.m = coords_npy.shape[0];
-//        madata.bbox = Box(Point(&coords_carray[0]), Point(&coords_carray[0]));
-//        for (int i = 0; i < madata.m; i++) {
-//            coords[i] = Point(&coords_carray[i * 3]);
-//            madata.bbox.addPoint(coords[i]);
-//        }
-//        coords_npy.destruct();
-//        std::cout << "bbox: " << madata.bbox.min[0] << " " << madata.bbox.min[1] << " " << madata.bbox.min[2] << " "
-//                  << madata.bbox.max[0] << " " << madata.bbox.max[1] << " " << madata.bbox.max[2] << std::endl;
-//
-//        madata.lfs = new float[madata.m];
-//
-//        madata.coords = &coords; // don't own this memory
-//        // madata.normals = &normals;
-//        madata.ma_coords = &ma_coords; // don't own this memory
-//
-//        madata.mask = new bool[madata.m];
-//
-//        // Perform the actual processing
-//        simplify_lfs(input_parameters, madata);
-//
-//        // count number of remaining points
-//        unsigned int cnt = 0;
-//        for (int i = 0; i < madata.m; i++)
-//            if (madata.mask[i]) cnt++;
-//        std::cout << cnt << " out of " << madata.m << " points remaining [" << int(100 * float(cnt) / madata.m) << "%]" << std::endl;
-//
-//        // Output results
-//        cnpy::npy_save(output_filtermask, madata.mask, shape, 1, "w");
-//        cnpy::npy_save(output_lfs, madata.lfs, shape, 1, "w");
-//
-//        // Free memory
-//        delete[] madata.mask;
-//        madata.mask = nullptr;
-//        delete[] madata.lfs;
-//        madata.lfs = nullptr;
-//        delete[] madata.ma_qidx;
-//        madata.ma_qidx = nullptr;
+        // Free memory
+        delete[] madata.mask;
+        madata.mask = nullptr;
+        delete[] madata.lfs;
+        madata.lfs = nullptr;
+        delete[] madata.ma_qidx;
+        madata.ma_qidx = nullptr;
 
 
     } catch (TCLAP::ArgException &e) { std::cerr << "Error: " << e.error() << " for " << e.argId() << std::endl; }
